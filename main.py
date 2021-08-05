c5fb1d18b81d95df8e43392cdc2a26ca87f0213d
Conover, Anna <aconover@teksystems.com>
2021-08-05 14:12:37 -0500

    Added code to connect to the existing database

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ for i in f.readlines():
     records.append(i.split("/ "))
 
 print(records)
+
+try:
+    connection = psycopg2.connect(database = "staff", user = "anna", password = "python", host = "127.0.0.1", port = "5432")
+
+except psycopg2.Error as err:
+    print("Yeah no")
+
+else:
+    print("Connection was successful")
